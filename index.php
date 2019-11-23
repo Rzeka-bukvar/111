91b9477549ccd23b8325eb058c21519b10638d0b
429271-9 <429271-9>
2019-11-23 13:33:13 +0300

    laba4

--- index.php
+++ index.php
@@ -12,4 +12,26 @@ include_once('core/LogInterface.php');
 include_once('RzekaMansur/Linear.php');
 include_once('RzekaMansur/Square.php');
 include_once('RzekaMansur/MyLog.php');
+
+
+echo "Vvedite 3 parametra A, B , C. \n";
+$paramens = explode(" ", fgets(STDIN));
+
+try {
+	if (count($paramens) != 3) {
+		throw new RzekaMansurException("You entered not 3 numbers. Try again.");
+	}
+	$a = (float)$paramens[0];
+	$b = (float)$paramens[1];
+	$c = (float)$paramens[2];
+	$square = new Square();
+	$temp = $square->solve($a, $b, $c);
+	MyLog::log("Korni uravneniya: ". implode(" , ", $temp));
+}
+catch (RzekaMansurException $e){
+	MyLog::log($e->GetMessage());
+}
+
+MyLog::write()."\n";
+
 ?>
