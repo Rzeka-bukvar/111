a6ac8f77ee1f2b57404f4fbdfee0f0a01f57ec24
Rzeka-bukvar <evgenius1609@gmail.com>
2019-12-13 00:53:50 +0300

    master

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Сборка проекта..."
-
-#Получение имени ветки
-vers = $(git symbolic-ref --short -q HEAD>version)
-$(git clone https://github.com/rok9ru/trpo-core core)
-echo "Все сделано!. Запустите программу"
